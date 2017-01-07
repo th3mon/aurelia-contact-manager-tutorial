cfb925117fa4d85649de2a8016189aeb65630c66
Przemysław Szelenberger <p.szelenberger@gmail.com>
2017-01-07 20:12:04 +0100

    Building Out the Default Route.

--- scripts/app-bundle.js
+++ scripts/app-bundle.js
@@ -93,6 +93,20 @@ define('resources/index',["exports"], function (exports) {
     //config.globalResources([]);
   }
 });
+define('no-selection',['exports'], function (exports) {
+    'use strict';
+
+    Object.defineProperty(exports, "__esModule", {
+        value: true
+    });
+    class NoSelection {
+        constructor() {
+            this.message = 'Please Select Contact.';
+        }
+    }
+    exports.NoSelection = NoSelection;
+});
 define('text!styles.css', ['module'], function(module) { module.exports = "body { padding-top: 70px; }\n\nsection {\n  margin: 0 20px;\n}\n\na:focus {\n  outline: none;\n}\n\n.navbar-nav li.loader {\n    margin: 12px 24px 0 6px;\n}\n\n.no-selection {\n  margin: 20px;\n}\n\n.contact-list {\n  overflow-y: auto;\n  border: 1px solid #ddd;\n  padding: 10px;\n}\n\n.panel {\n  margin: 20px;\n}\n\n.button-bar {\n  right: 0;\n  left: 0;\n  bottom: 0;\n  border-top: 1px solid #ddd;\n  background: white;\n}\n\n.button-bar > button {\n  float: right;\n  margin: 20px;\n}\n\nli.list-group-item {\n  list-style: none;\n}\n\nli.list-group-item > a {\n  text-decoration: none;\n}\n\nli.list-group-item.active > a {\n  color: white;\n}\n"; });
 define('text!app.html', ['module'], function(module) { module.exports = "<template>\n    <require from=\"bootstrap/css/bootstrap.css\"></require>\n    <require from=\"./styles.css\"></require>\n\n    <nav class=\"navbar navbar-default navbar-fixed-top\">\n        <div class=\"navbar-header\">\n            <a href=\"#\" class=\"navbar-brand\">\n                <i class=\"fa fa-user\"></i>\n                <span>\n                    Contacts\n                </span>\n            </a>\n        </div>\n    </nav>\n\n    <div class=\"container\">\n        <div class=\"row\">\n            <div class=\"col-md-4\">\n                Contact List Placeholder\n            </div>\n            <router-view class=\"col-md-8\"></router-view>\n        </div>\n    </div>\n</template>\n"; });
+define('text!no-selection.html', ['module'], function(module) { module.exports = "<template>\n  <div class=\"no-selection text-center\">\n    <h2>${message}</h2>\n  </div>\n</template>\n"; });
 //# sourceMappingURL=app-bundle.js.map
